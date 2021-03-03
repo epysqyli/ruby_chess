fbc4d85f20742cf13a30cea51b8d05204696a75b
epysqyli <elvis.pysqyli@gmail.com>
2021-03-03 18:55:11 +0100

    Add initial position RSpec example

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,10 +6,16 @@ describe ChessBoard do
     subject(:clean_board) { described_class.new }
 
     context 'when the board is first created' do
-      it 'places the pieces in their correct places' do
+      it 'places the white king in its correct place' do
         board = clean_board.instance_variable_get(:@board)
         white_king_pos = board.detect { |s| s.state == "\u265A" }
         expect([white_king_pos.x, white_king_pos.y]).to eq([5, 1])
+      end
+
+      it 'places the black quuen in its correct place' do
+        board = clean_board.instance_variable_get(:@board)
+        black_queen_pos = board.detect { |s| s.state == "\u2655" }
+        expect([black_queen_pos.x, black_queen_pos.y]).to eq([4, 8])
       end
     end
   end
